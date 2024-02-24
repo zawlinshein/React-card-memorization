1003dd3d34f4f0380e822ffebf93664e3f21c896
zawlinshein <pudgeismyfavourate@gmail.com>
2024-02-24 13:41:37 +0630

    fixed type in custombutton

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,24 +1,28 @@
-import React, { forwardRef } from "react"
+import React from "react"
+import * as Dialog from '@radix-ui/react-dialog'
 
 
-export type Props = React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
-  children: string | React.ReactNode
-  rightIcon?: React.ReactNode
-  leftIcon?: React.ReactNode
-}
 
-const CustomButton = forwardRef<HTMLDivElement, Props>((props, ref) => {
+export type Props = Dialog.DialogTriggerProps
+  & React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
+  & {
+    children: string | React.ReactNode
+    rightIcon?: React.ReactNode
+    leftIcon?: React.ReactNode
+  }
+
+const CustomButton = (props: Props) => {
 
   const { children, leftIcon, rightIcon, ...other } = props
 
   return (
-    <div ref={ref} className="button-19" {...other} >
+    <div className="button-19" {...other} >
       {leftIcon}
       {children}
       {rightIcon}
       <div className="button-background" ></div>
     </div>
   )
-})
+}
 
 export default CustomButton
